e188c89b40592b6de22a9bfe396a72f8b17f6af6
Enrique Saracho Felix <esarachof97@gmail.com>
2022-07-10 12:57:57 -0600

    Update app.js index.html

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div>
+            <h1>Section 1 - Solo Project</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app"
 
 ReactDOM.render(
-  <h1>Section 1 - Solo Project</h1>,
+  <App />,
   document.getElementById("root")
 );
