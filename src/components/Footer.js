4648f8522f5cd7e0504f825b19fb15766fb0795c
Enrique Saracho Felix <esarachof97@gmail.com>
2022-07-10 18:26:47 -0600

    Update app.js style.css Footer.js
    Updated app.js style.css and Created Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <button className="footer--btn-facebook">
+        <i class="fab fa-facebook-f"></i>
+      </button>
+      <button className="footer--btn-instagram">
+        <i class="fab fa-instagram"></i>
+      </button>
+      <button className="footer--btn-github">
+        <i class="fab fa-github"></i>
+      </button>
+    </div>
+  );
+}
